3119bc87ea7790f897e0e021d939e6c56bc5ccc2
Alhad Manure <60777121+Alhad-Manure@users.noreply.github.com>
2024-07-09 22:24:59 +0530

    Add files via upload

--- SetUp.bat
+++ SetUp.bat
@@ -8,14 +8,11 @@ call venv\Scripts\activate
 REM Install Flask
 call pip install Flask
 
-REM Install bson
-call pip install bson
-
 REM Install pymongo
 call pip install pymongo
 
 REM Set FLASK_APP environment variable
-set FLASK_APP=app.py
+set FLASK_APP=DataBasePersistenceApp.py
 
 REM Run Flask app
 call flask run --host=0.0.0.0
